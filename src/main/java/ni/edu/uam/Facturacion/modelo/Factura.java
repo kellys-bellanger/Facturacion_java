98fbc43f34c3e0d767a773f21b0da888190ea1e6
kellys bellanger <kbbellanger@uamv.edu.ni>
2026-05-26 12:03:23 -0600

    Agrego cambios

--- src/main/java/ni/edu/uam/Facturacion/modelo/Factura.java
+++ src/main/java/ni/edu/uam/Facturacion/modelo/Factura.java
@@ -2,11 +2,9 @@ package ni.edu.uam.Facturacion.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
+import ni.edu.uam.Facturacion.calculadores.CalculadoraSiguienteNumeroParaAnyo;
 import org.hibernate.annotations.GenericGenerator;
-import org.openxava.annotations.DefaultValueCalculator;
-import org.openxava.annotations.Hidden;
-import org.openxava.annotations.Required;
-import org.openxava.annotations.Stereotype;
+import org.openxava.annotations.*;
 import org.openxava.calculators.CurrentLocalDateCalculator;
 
 import javax.persistence.*;
@@ -26,7 +24,9 @@ public class Factura {
     int anyo;
 
     @Column(length = 6)
-    String numero;
+            @DefaultValueCalculator(value = CalculadoraSiguienteNumeroParaAnyo.class,
+                    properties = @PropertyValue(name = "anyo"))
+    int numero;
 
     @Required
     @DefaultValueCalculator(CurrentLocalDateCalculator.class)
